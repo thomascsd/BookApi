81679a15e60ec0fc99fef8fe1ce6c809684ba59b
thomascsd <thomascsd808@gmail.com>
2021-02-14 18:48:34 +0800

    加入專案檔案。

---
+++ BookApi.EF/BookContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookApi.EF
+{
+   public class BookContext: DbContext
+    {
+
+
+    }
+}
