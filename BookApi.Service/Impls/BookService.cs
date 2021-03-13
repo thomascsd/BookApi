44bd53d1f6d699349e0af07575cf1975204b317f
thomascsd <thomascsd808@gmail.com>
2021-03-13 09:38:18 +0800

    GetBooks

--- BookApi.Service/Impls/BookService.cs
+++ BookApi.Service/Impls/BookService.cs
@@ -1,13 +1,21 @@
 ﻿using BookApi.Core.Dtos.Books;
+using BookApi.DataAccess;
 using BookApi.Service.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace BookApi.Service.Impls
 {
     public class BookService : IBookService
     {
+        private BookContext m_context;
+        public BookService(BookContext context)
+        {
+            this.m_context = context;
+        }
+
         public void AddBook()
         {
             throw new NotImplementedException();
@@ -15,7 +23,18 @@ namespace BookApi.Service.Impls
 
         public List<GetBooksDto> GetBooks()
         {
-            throw new NotImplementedException();
+            var books = (from b in this.m_context.Books
+                         join w in this.m_context.Writers
+                         on b.WriterID equals w.WriterID
+                         select new GetBooksDto
+                         {
+                             BookID = b.BookID,
+                             Description = b.Description,
+                             Title = b.Title,
+                             WriterName = w.Name
+                         }).ToList();
+
+            return books;
         }
     }
 }
